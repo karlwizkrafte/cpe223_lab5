027b19efc529212126278d5ef3617c34b84054d7
Karl Vince Reyes <98654168+karlwizkrafte@users.noreply.github.com>
2026-04-27 17:09:44 +0800

    Add button functionalities

--- src/main/java/cpe223/karlvince/lab5/CalculatorController.java
+++ src/main/java/cpe223/karlvince/lab5/CalculatorController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -25,29 +26,76 @@ public class CalculatorController {
     @FXML
     public void initialize() {
 
+
+
         root.getStyleClass().add("root");
         buttonContainer.getStyleClass().add("container");
         tField.getStyleClass().add("tf");
         
+
+        // TextField Modifier
+
+        tField.setPrefHeight(100);
+        tField.setMinHeight(100);
+        tField.setMaxHeight(100);
+
+        tField.setTextFormatter(new TextFormatter<>(change -> {
+            if (change.getControlNewText().length() <= 16) {
+                return change;
+            }
+            return null;
+        }));
+
+        tField.textProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal.length() > 9) {
+                tField.setStyle("-fx-font-size: 30px;");
+            } else {
+                tField.setStyle("-fx-font-size: 50px;");
+            }
+        });
+
         buttonContainer.setHgap(0);
         buttonContainer.setVgap(0);
         for (int row = 0; row < BUTTONS.length; row++) {
             for (int col = 0; col < BUTTONS[row].length; col++) {
                 Button btn = new Button(BUTTONS[row][col]);
-                styleBtn(btn, row, col);
+                modifyBtn(btn, row, col, BUTTONS[row][col]);
                 buttonContainer.add(btn, col, row);     
             }
         }
-
     }
 
-    private void styleBtn(Button btn, int row, int col) {
-        String styleNameID = String.format("btn_%d_%d", col, row); System.out.println(styleNameID);
+    private void modifyBtn(Button btn, int row, int col, String label) {
+
+        String styleNameID = String.format("[Calculator Controller] Assigned Style for %s: btn_%d_%d", label, col, row); System.out.println(styleNameID);
+
         btn.getStyleClass().add(styleNameID);
         btn.getStyleClass().add("calculatorbtn");
         btn.prefWidthProperty().bind(buttonContainer.widthProperty().multiply(1.0 / BUTTONS[0].length));
         btn.prefHeightProperty().bind(buttonContainer.heightProperty().multiply(0.2));
+        btn.setOnAction(e -> {
+            handButtonClick(label);
+        });
+
         double margin = 1.5;
         GridPane.setMargin(btn, new Insets(margin,margin,margin,margin));
+    }
+
+    private void handButtonClick(String label) {
+        switch (label) {
+
+            case "C"    -> tField.clear();
+            case "."    -> tField.appendText(label);
+            case "+"    -> tField.appendText(label);
+            case "-"    -> tField.appendText(label); 
+            case "×"    -> tField.appendText(label);
+            case "÷"    -> tField.appendText(label);
+
+            default     -> {
+                if (label.matches("[0-9]")) {
+                    tField.appendText(label);
+                }
+            }
+        }
     }
 }
