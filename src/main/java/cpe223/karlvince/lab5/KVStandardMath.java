06c8a01aae0390974ca99e631cc39d4e26aba0c3
Karl Vince Reyes <98654168+karlwizkrafte@users.noreply.github.com>
2026-04-27 16:11:33 +0800

    Modify for pure math operations only

--- src/main/java/cpe223/karlvince/lab5/KVStandardMath.java
+++ src/main/java/cpe223/karlvince/lab5/KVStandardMath.java
@@ -1,61 +1,52 @@
 package cpe223.karlvince.lab5;
 
-import javafx.scene.control.TextField;
-
 // [Version 0.0.1]
 public class KVStandardMath {
 
-    public static double add(TextField... inputs) {
+    public static double add(double... values) {
         double sum = 0;
-
-        try {
-            for (TextField in : inputs) {
-                sum += Double.parseDouble(in.getText());
-            }
-            return sum;
-
-        } catch (NumberFormatException err) {
-            return Double.NaN;
+        for (double val : values) {
+            sum += val;
         }
+        return sum;
     }
 
-    public static double minus(TextField... inputs) {
-        try {
-            double result = Double.parseDouble(inputs[0].getText());
-            for (int i = 1; i < inputs.length; i++) {
-                result -= Double.parseDouble(inputs[i].getText());
-            }
-            return result;
-        } catch (NumberFormatException | ArrayIndexOutOfBoundsException err) {
+    public static double minus(double... values) {
+        if (values.length == 0)
             return Double.NaN;
+        double result = values[0];
+        for (int i = 1; i < values.length; i++) {
+            result -= values[i];
         }
+        return result;
     }
 
-    public static double multiply(TextField... inputs) {
-        try {
-            double result = 1;
-            for (TextField in : inputs) {
-                result *= Double.parseDouble(in.getText());
-            }
-            return result;
-        } catch (NumberFormatException err) {
-            return Double.NaN;
+    public static double multiply(double... values) {
+        double result = 1;
+        for (double val : values) {
+            result *= val;
         }
+        return result;
     }
 
-    public static double divide(TextField... inputs) {
-        try {
-            double result = Double.parseDouble(inputs[0].getText());
-            for (int i = 1; i < inputs.length; i++) {
-                double divisor = Double.parseDouble(inputs[i].getText());
-                if (divisor == 0)
-                    return Double.NaN;
-                result /= divisor;
-            }
-            return result;
-        } catch (NumberFormatException | ArrayIndexOutOfBoundsException err) {
+    public static double divide(double... values) {
+        if (values.length == 0)
             return Double.NaN;
+        double result = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] == 0)
+                return Double.NaN;
+            result /= values[i];
         }
+        return result;
+    }
+
+    public static double negate(double value) {
+        return -value;
+    }
+
+    public static double percentage(double value) {
+        return value / 100.0;
     }
 
 }
