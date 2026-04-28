4d293fd1a2c3fa66d752a56d2ae675efaef6a71f
Karl Vince Reyes <98654168+karlwizkrafte@users.noreply.github.com>
2026-04-29 00:57:14 +0800

    Add simple logo

--- src/main/java/cpe223/karlvince/lab5/App.java
+++ src/main/java/cpe223/karlvince/lab5/App.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import kaviyeslabs.java.graphics.ZhanaFX;
@@ -24,10 +25,12 @@ public class App extends Application {
         scene = new Scene(loadFXML("calculator_main"), SCX, SCY);
         scene.getStylesheets().add(Toolbox.respath("/cpe223/karlvince/lab5/themes/1.css"));
 
+        Image icon = new Image(getClass().getResourceAsStream("/cpe223/karlvince/lab5/icons/KVCal-64px.png"));
+        
         stage.setTitle("Calculator");
         stage.setMinWidth(SCX + 10);
         stage.setMinHeight(SCY + 15);
-
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
 
